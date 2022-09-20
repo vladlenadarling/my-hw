2dee168cac3920b677ba0fd233d3864e03e9541e
Vladlena <vladlena3637@gmail.com>
2022-09-20 16:25:17 +0300

    add void

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] mass1 = new string[] { "hello", "2", ";-)" };
+string[] mass2 = new string[mass1.Length];
+int j = 0;
+
+void CheckMass(string[] mass1, string[] mass2)
+{
+    for (int i = 0; i < mass1.Length; i++)
+    {
+        if (mass1[i].Length <= 3)
+        {
+            mass2[j] = mass1[i];
+            j++;
+        }
+    }
+}
